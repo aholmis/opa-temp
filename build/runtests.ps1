d06e6f7b269e02f7b82b496646d86dda5738f4cc
Anders Holmefjord <Anders.Holmefjord@dnvgl.com>
2021-03-30 11:51:35 +0200

    Trying to set build status message

--- build/runtests.ps1
+++ build/runtests.ps1
@@ -1,8 +1,9 @@
 $ErrorActionPreference = "Stop"
 
-.\build\opa.exe test .\rules -v
+.\build\opa.exe test .\rules -v --format=pretty
 
 if($LASTEXITCODE -ne 0)
 {
+    ##teamcity[buildStatus status='FAILURE' text='{build.status.text}: Test(s) failed.']
     write-error "Test failure detected."
 }
