90f07c4b5d2da8f506e44c66c2c09f9d38f3a4bd
Anders Holmefjord <Anders.Holmefjord@dnvgl.com>
2021-03-30 14:25:12 +0200

    Test status 3

--- build/runtests.ps1
+++ build/runtests.ps1
@@ -1,9 +1,6 @@
-$ErrorActionPreference = "Stop"
-
 .\build\opa.exe test .\rules -v --format=pretty
 
 if($LASTEXITCODE -ne 0)
 {
     "##teamcity[buildStatus status='FAILURE' text='{build.status.text}: Test(s) failed.']"
-    # write-error "Test failure detected."
 }
